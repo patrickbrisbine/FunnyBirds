2e771abe91b51dfce359edd838ef6787560028b3
Patrick Brisbine <patrickbrisbine@gmail.com>
2017-12-22 14:07:27 -0800

    Updating license text

--- Assets/Scripts/DamageTarget.cs
+++ Assets/Scripts/DamageTarget.cs
@@ -1,5 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/* 
+ * Peril Beasts Copyright (C) Patrick Brisbine - All Rights Reserved
+ *
+ * Peril Beasts is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-nc-nd/3.0/>.
+ *  
+ * Written by Patrick Brisbine December 2017
+ */
+
 using UnityEngine;
 
 public class DamageTarget : MonoBehaviour
